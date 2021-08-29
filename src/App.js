299ba507df9870c4acbc0aba06506b73986cfbe8
jawad <jawadhalbymakary@gmail.com>
2021-08-30 01:44:38 +0300

    created the design of the header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { lazy, Suspense } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import * as ROUTES from './constants/routes'
+import userContext from './context/user';
+import useAuthListener from './hooks/use-auth-listener';
 // allow us to split bundle into chunks
 const Login=lazy(()=>import('./pages/Login'))
 const Signup=lazy(()=>import('./pages/Signup'))
 const NotFound=lazy(()=>import('./pages/NotFound'))
 const Dashboard=lazy(()=>import('./pages/Dashboard'))
 function App() {
+  const {user}=useAuthListener()
   return (
+    <userContext.Provider value={{user}}>
+
     <Router>
       <Suspense fallback={<p>Loading ...</p>}>
-
-    
       <Switch>
         <Route path={ROUTES.LOGIN} component={Login} />  
         <Route path={ROUTES.SIGN_UP} component={Signup} />    
@@ -20,6 +23,8 @@ function App() {
         </Switch>
       </Suspense>
     </Router>
+    </userContext.Provider>
+
   );
 }
 
