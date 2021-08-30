c304cebe17855e2254c32bacb1d964390924dd7e
jawad <jawadhalbymakary@gmail.com>
2021-08-30 04:36:58 +0300

    added the use user hook

---
+++ src/hooks/use-user.js
@@ -0,0 +1,18 @@
+import { useState,useEffect,useContext } from "react";
+import userContext from "../context/user";
+import { getUserByUserId } from "../services/firebase";
+export default function useUser(){
+    const[activeUser,setActiveUser]=useState('')
+    const {user}=useContext(userContext)
+    useEffect(()=>{
+        async function getUserObjectByUserId(){
+            // fct that get user data based on id
+            const response = await getUserByUserId(user.uid)
+            setActiveUser(response)
+        }
+        if(user?.uid){
+            getUserObjectByUserId()
+        }
+    },[user])
+    return {user:activeUser}
+}
