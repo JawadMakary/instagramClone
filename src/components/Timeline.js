52f232f58ad0c0009ad7765322e6026c66cf23c8
jawad <jawadhalbymakary@gmail.com>
2021-09-07 02:46:23 +0300

    skeleton refactore for timeline cmp

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -7,9 +7,9 @@ const Timeline = () => {
         <div className='container col-span-2'>
             {!photos ? (
                 <>
-                {[... new Array(4)].map((_,index)=>
-                <Skeleton key={index} count={1} width={320} height={400} />
-                )}
+               
+                <Skeleton  count={4} width={640} height={500} className='mb-5' />
+               
                 </>
             )
             : photos?.length > 0 ? (
